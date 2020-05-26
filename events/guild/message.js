4d5ceede4f74878ac23eb0833f5409924383c323
Vaibhav Patel <meisilre@gmail.com>
2020-05-26 17:25:21 -0400

    remvoed vai added stfu

--- events/guild/message.js
+++ events/guild/message.js
@@ -39,7 +39,7 @@ module.exports = async (client, message) => {
 
     if(message.content.toLowerCase().toString() === "fuck") return message.channel.send("fuck indeed");
 
-    if(message.content.toLowerCase().includes("vai")) return message.channel.send("I strongly advise you to refrain from using master's name and instead refer to him as master or daddy 🤤😛😝🥰");
+    if((message.content.toLowerCase().includes('shut up'))  || (message.content.toLowerCase().includes('stfu')) || (message.content.toLowerCase().includes('shut the fuck up')) || (message.content.toLowerCase().toString() === "shut up") || (message.content.toLowerCase().toString() === "stfu") || (message.content.toLowerCase().toString() === "shut the fuck up")) return message.channel.send(`${message.author.username} you shut the fuck up`);
 
     if (cmd.length === 0) return;
 
